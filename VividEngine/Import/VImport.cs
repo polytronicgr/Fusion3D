9d488531f9eeda0738982f699e43db2e8a66ea37
Antony Wells <antonyrobwells@gmail.com>
2018-10-31 03:00:59 +0000

    Update VividEngine/Import/VImport.cs

--- VividEngine/Import/VImport.cs
+++ VividEngine/Import/VImport.cs
@@ -8,44 +8,44 @@ namespace Vivid3D.Import
     {
         public static Dictionary<string, Importer> Imports = new Dictionary<string, Importer>();
 
-        public static void RegDefaults()
+        public static void RegDefaults ( )
         {
-            RegImp(".3ds", new AssImpImport());
-            RegImp(".fbx", new AssImpImport());
-            RegImp(".blend", new AssImpImport());
-            RegImp(".dae", new AssImpImport());
-            RegImp(".b3d", new AssImpImport());
-            RegImp(".gltf", new AssImpImport());
-            RegImp(".x", new AssImpImport());
+            RegImp ( ".3ds" , new AssImpImport ( ) );
+            RegImp ( ".fbx" , new AssImpImport ( ) );
+            RegImp ( ".blend" , new AssImpImport ( ) );
+            RegImp ( ".dae" , new AssImpImport ( ) );
+            RegImp ( ".b3d" , new AssImpImport ( ) );
+            RegImp ( ".gltf" , new AssImpImport ( ) );
+            RegImp ( ".x" , new AssImpImport ( ) );
         }
 
-        public static void RegImp(string key, Importer imp)
+        public static void RegImp ( string key , Importer imp )
         {
-            Imports.Add(key, imp);
+            Imports.Add ( key , imp );
         }
 
-        public static Importer GetImp(string key)
+        public static Importer GetImp ( string key )
         {
-            if (Imports.ContainsKey(key))
+            if ( Imports.ContainsKey ( key ) )
             {
-                return Imports[key];
+                return Imports [ key ];
             }
             return null;
         }
 
-        public static GraphNode3D ImportAnimNode(string path)
+        public static GraphNode3D ImportAnimNode ( string path )
         {
             string key = new FileInfo(path).Extension.ToLower();
-            var imp = Imports[key];
-            var r = imp.LoadAnimNode(path);
+            Importer imp = Imports[key];
+            GraphNode3D r = imp.LoadAnimNode(path);
             return r;
         }
 
-        public static GraphNode3D ImportNode(string path)
+        public static GraphNode3D ImportNode ( string path )
         {
             string key = new FileInfo(path).Extension.ToLower();
-            var imp = Imports[key];
-            var r = imp.LoadNode(path);
+            Importer imp = Imports[key];
+            GraphNode3D r = imp.LoadNode(path);
             return r;
         }
     }

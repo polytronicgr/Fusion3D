676f1246a023463fb1543b6c4e3a6c618a21b679
Antony Wells <antonyrwellsunity@gmail.com>
2018-11-06 21:50:35 +0000

    generalized search algos for future improvement.

--- VividScript/CodeScope.cs
+++ VividScript/CodeScope.cs
@@ -1,63 +1,39 @@
-﻿using System.Collections.Generic;
-using VividScript.VStructs;
+﻿using VividScript.VStructs;
 
 namespace VividScript
 {
     public class CodeScope
     {
         public CodeScope OutterScope = null;
-        public List<VSVar> LocalVars = new List<VSVar>();
 
-        public List<FuncLink> LocalFuncs = new List<FuncLink>();
+        public LinkEngineBase Linker = null;
 
-        public void RegisterFunc ( FuncLink func )
+        public void RegisterFunc ( FuncLink link )
         {
-            LocalFuncs.Add ( func );
+            Linker.RegisterFunc ( link );
         }
 
         public void RegisterVar ( VSVar var )
         {
-            LocalVars.Add ( var );
+            Linker.RegisterVar ( var );
         }
 
-        public FuncLink FindFunc ( string name, bool searchOutter )
+        public FuncLink FindFunc ( string name, bool outterScope )
         {
-            foreach ( FuncLink func in LocalFuncs )
-            {
-                if ( func.Name == name )
-                {
-                    return func;
-                }
-            }
-            if ( searchOutter )
-            {
-                if ( OutterScope != null )
-                {
-                    return OutterScope.FindFunc ( name, true );
-                }
-            }
-            VME.Main.Error ( "Could not find func called:" + name, "ScopeError" );
-            return null;
+            return Linker.FindFunc ( name, outterScope );
         }
 
-        public VSVar FindVar ( string name, bool searchOutter )
+        public VSVar FindVar ( string name, bool outterScope )
         {
-            foreach ( VSVar v in LocalVars )
+            return Linker.FindVar ( name, outterScope );
+        }
+
+        public CodeScope ( )
+        {
+            Linker = new LinkEngineBase ( )
             {
-                if ( v.Name == name )
-                {
-                    return v;
-                }
-            }
-            if ( searchOutter )
-            {
-                if ( OutterScope != null )
-                {
-                    return OutterScope.FindVar ( name, true );
-                }
-            }
-            VME.Main.Error ( "Could not find variable called:" + name, "ScopeError" );
-            return null;
+                Scope = this
+            };
         }
     }
 }
---
+++ VividScript/LinkEngineBase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using VividScript.VStructs;
+
+namespace VividScript
+{
+    public class LinkEngineBase
+    {
+        public CodeScope Scope = null;
+
+        public List<VSVar> LocalVars = new List<VSVar>();
+
+        public List<FuncLink> LocalFuncs = new List<FuncLink>();
+
+        public virtual void RegisterFunc ( FuncLink func )
+        {
+            LocalFuncs.Add ( func );
+        }
+
+        public virtual void RegisterVar ( VSVar var )
+        {
+            LocalVars.Add ( var );
+        }
+
+        public virtual FuncLink FindFunc ( string name, bool searchOutter )
+        {
+            foreach ( FuncLink func in LocalFuncs )
+            {
+                if ( func.Name == name )
+                {
+                    return func;
+                }
+            }
+            if ( searchOutter )
+            {
+                if ( Scope.OutterScope != null )
+                {
+                    return Scope.OutterScope.FindFunc ( name, true );
+                }
+            }
+            VME.Main.Error ( "Could not find func called:" + name, "ScopeError" );
+            return null;
+        }
+
+        public virtual VSVar FindVar ( string name, bool searchOutter )
+        {
+            foreach ( VSVar v in LocalVars )
+            {
+                if ( v.Name == name )
+                {
+                    return v;
+                }
+            }
+            if ( searchOutter )
+            {
+                if ( Scope.OutterScope != null )
+                {
+                    return Scope.OutterScope.FindVar ( name, true );
+                }
+            }
+            VME.Main.Error ( "Could not find variable called:" + name, "ScopeError" );
+            return null;
+        }
+    }
+}
